e96afad5e5fdcc362f1c9f70a198e8a58490b1c3
苏铭轩 <sss@SSS.local>
2020-11-03 16:39:14 +0800

    优化代码，添加查询方法

--- lib/hash_map_attributes.rb
+++ lib/hash_map_attributes.rb
@@ -2,30 +2,46 @@ require "hash_map_attributes/version"
 
 module HashMapAttributes
   def self.included(base)
-    base.extend ClassMethods    
+    base.extend ClassMethods
   end
 
   module ClassMethods
     def hash_map_attributes(*attributes, to:)
       _hash_map_attributes
       attributes.each do |attribute|
-        self.instance_variable_get('@_hash_map_attributes') << attribute.to_s
+        self.instance_variable_get('@_hash_map_attributes')[to] ||= Set.new
+        self.instance_variable_get('@_hash_map_attributes')[to] << attribute.to_s
       end
       class_eval do
-        _hash_map_attributes.each do |hma|
-          define_method hma do
-            send(to).to_hash.stringify_keys![hma]
-          end
+        _hash_map_attributes.each_pair do |_to, _attributes|
+          _attributes.each do |_attribute|
+            define_method _attribute do
+              send(_to).to_hash.stringify_keys![_attribute]
+            end
 
-          define_method "#{hma}=" do |v|
-            send(to).to_hash.stringify_keys![hma] = v
+            define_method "#{_attribute}=" do |v|
+              send(_to).to_hash.stringify_keys![_attribute] = v
+            end
           end
         end
       end
     end
 
+    def where_hash(**options)
+      arr = []
+      class_name = model_name.plural
+      options.each_pair do |k, v|
+        to = _hash_map_attributes.select { |a, b| b.include?(k.to_s) }.keys.first
+        next if to.nil?
+
+        arr << "#{class_name}.#{to.to_s}->>'#{k}' = '#{v}'"
+      end
+      sql = arr.join(' and ')
+      where(sql)
+    end
+
     def _hash_map_attributes
-      @_hash_map_attributes ||= Set.new
+      @_hash_map_attributes ||= {}
     end
   end
 end
